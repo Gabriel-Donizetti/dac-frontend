412fb217203272e03a6e15ea90418d206607f0ff
unknown <guilherme.viol1914@hotmail.com>
2025-05-18 18:23:37 -0300

    feat/inicio gateway e auth

--- backend/wsauth/src/main/java/com/example/wsauth/wsauth/WsauthApplication.java
+++ backend/wsauth/src/main/java/com/example/wsauth/wsauth/WsauthApplication.java
@@ -1,13 +1,13 @@
-package com.dac.msauth;
+package com.example.wsauth.wsauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MsauthApplication {
+public class WsauthApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsauthApplication.class, args);
+		SpringApplication.run(WsauthApplication.class, args);
 	}
 
 }
